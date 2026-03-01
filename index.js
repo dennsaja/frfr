7224f9b6c97de6c622248d7c5e43810a31d3c901
Denis <136689391+dennsaja@users.noreply.github.com>
2026-03-01 16:36:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// Import packages
+const express = require("express");
+const path = require('path');
+const errorsys = require("./routes/error-system");
+const index = require("./routes/index");
+
+// Middlewares
+const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+
+// Routes
+
+app.use("/", index);
+app.use("/err", errorsys);
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'notfound.json'));
+});
+
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
